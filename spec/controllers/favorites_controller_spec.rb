a17eb422234129c3d9916dfd71ef4ff10011df08
Maura Fitzgerald <maurafitzgerald@Mauras-MacBook-Pro.local>
2013-12-05 19:44:32 -0800

    finis

--- spec/controllers/favorites_controller_spec.rb
+++ spec/controllers/favorites_controller_spec.rb
@@ -8,30 +8,15 @@ describe FavoritesController do
 
   subject   { response }
 
-  describe "GET 'index'" do
-  before(:each) { get :index, :format => :json, :user_id => 1}
-
-  its(:body) { should_not be_empty}
-  its(:status) { should be 200}
-  end
-
-  describe "POST 'create'" do
-    before(:each) { post :create, favorite_attrs }
-
-    its(:body)    { should_not be_empty }
-    its(:status)  { should be 201 }
-
-    context "with bad params" do
-      before(:each) {post :create, bad_favorite_attrs}
-
-      its(:body) { should_not be_empty}
-      its(:status) {should be 422}
-    end
-  end
+describe "POST 'create'" do
+  before(:each) { post :create, {:user_id => favorite.user_id, :name => "rando"}, :format => :json}
+end
 
 context "existing favorite" do
+
   let!(:favorite) {create :favorite}
   let(:favorite_attrs) {{ :id => favorite.id, :format => :json, :favorite => {:name => "random favorite", :user_id => favorite.user_id}}}
+  let(:bad_favorite_attrs) {{ :id => favorite.id, :favorite => { :name => nil}, :format => :json}}
 
   describe "GET 'show'" do
     before(:each) { get :show, :id => favorite.id, :format => :json }
@@ -39,7 +24,7 @@ context "existing favorite" do
     its(:body)    { should_not be_empty }
     its(:status)  { should be 200}
 
-    context "with an unknown favorite" do
+    context "unknown favorite" do
       before(:each) { get :show, :id => 9999, :format => :json }
 
       its("body.strip") { should be_empty }
@@ -50,19 +35,11 @@ context "existing favorite" do
   describe "PUT 'update'" do
     before(:each) { put :update, favorite_attrs }
 
-    its("body.strip") { should be_empty }
-    its(:status)      { should be 204 }
+    context "with bad parameters" do
+      before(:each) {put :update, bad_favorite_attrs}
 
-    it "updates the favorite" do
-      Favorite.find(favorite.id).name.should eq(favorite_attrs[:favorite][:name])
-      Favorite.find(favorite.id).user_id.should eq(favorite_attrs[:favorite][:user_id])
-    end
-
-    context "with bad params" do
-      before(:each) {put :update, {:id => favorite.id, :favorite => {:name => nil, :user_id => nil}, :format => :json}}
-
-      its(:body) {should_not be_empty}
-      its(:status) {should be 422}
+      its(:body) { should_not be_empty }
+      its(:status) { should be 422 }
     end
   end
 
